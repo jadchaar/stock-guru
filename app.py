dcaeaff2088a993e54ed9455a899e23badea48e1
jadchaar <jad.chaar@gmail.com>
2018-01-23 00:18:32 -0500

    removed attachment features

--- app.py
+++ app.py
@@ -74,30 +74,21 @@ def handleMessage(sender_psid, received_message):
 def webhook():
     # Parse the request body from the POST
     body = request.get_json()
-    # print(body)
-    # pp = pprint.PrettyPrinter()
-    # pp.pprint(body)
+    print(body)
 
     # Check the webhook event is from a Page subscription
     if body['object'] == 'page':
         # Iterate over each entry - there may be multiple if batched
         for entry in body['entry']:
-            # pp.pprint(entry)
-            # Get the webhook event. entry.messaging is an array, but
-            # will only ever contain one event, so we get index 0
-            webhook_event = entry['messaging'][0]
-            print(webhook_event)
-            
-            # Get the sender PSID
-            sender_psid = webhook_event['sender']['id']
-            print(f'Sender PSID: {sender_psid}')
+            for webhook_event in entry['messaging']:
+                # Get the sender PSID
+                sender_psid = webhook_event['sender']['id']
+                print(f'Sender PSID: {sender_psid}')
 
-            # Check if the event is a message or postback and
-            # pass the event to the appropriate handler function
-            if webhook_event['message']:
-                handleMessage(sender_psid, webhook_event['message'])   
-            elif webhook_event['postback']:
-                handlePostback(sender_psid, webhook_event['postback'])
+                # Check if the event is a message or postback and
+                # pass the event to the appropriate handler function
+                if webhook_event['message']:
+                    handleMessage(sender_psid, webhook_event['message'])
 
         # Return a '200 OK' response to all events
         return 'EVENT_RECEIVED', 200
@@ -106,25 +97,58 @@ def webhook():
         return '', 404
 
 
+# @app.route('/', methods=['POST'])
+# def webhook():
+
+#     # endpoint for processing incoming messaging events
+
+#     data = request.get_json()
+#     log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+#     if data["object"] == "page":
+
+#         for entry in data["entry"]:
+#             for messaging_event in entry["messaging"]:
+
+#                 if messaging_event.get("message"):  # someone sent us a message
+
+#                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+#                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+#                     message_text = messaging_event["message"]["text"]  # the message's text
+
+#                     send_message(sender_id, "roger that!")
+
+#                 if messaging_event.get("delivery"):  # delivery confirmation
+#                     pass
+
+#                 if messaging_event.get("optin"):  # optin confirmation
+#                     pass
+
+#                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+#                     pass
+
+#     return "ok", 200
+
+
 # def send_message(recipient_id, message_text):
 
-#     log('sending message to {recipient}: {text}'.format(recipient=recipient_id, text=message_text))
+#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
 #     params = {
-#         'access_token': os.environ.get('PAGE_ACCESS_TOKEN')
+#         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
 #     }
 #     headers = {
-#         'Content-Type': 'application/json'
+#         "Content-Type": "application/json"
 #     }
 #     data = json.dumps({
-#         'recipient': {
-#             'id': recipient_id
+#         "recipient": {
+#             "id": recipient_id
 #         },
-#         'message': {
-#             'text': message_text
+#         "message": {
+#             "text": message_text
 #         }
 #     })
-#     r = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, headers=headers, data=data)
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 #     if r.status_code != 200:
 #         log(r.status_code)
 #         log(r.text)
